29baa1e3d7c56f79e397b7bd570a2fa22783858c
Jeff Turner <Jeffrey.L.Turner@me.com>
2015-01-27 18:05:43 -0800

    fixed up eslint errs

--- src/scripts/services/aside.js
+++ src/scripts/services/aside.js
@@ -7,30 +7,30 @@ angular.module('ngAside')
    * @function
    */
   .factory('$aside', function($modal) {
-    'use strict';
-    var defaults = this.defaults = {
-      placement: 'left'
-    },
+     'use strict';
+     var defaults = this.defaults = {
+       placement: 'left'
+     },
 
-       asideFactory = {
-      // override open method
-      open: function(config) {
-        var options = angular.extend({}, defaults, config),
-            vertHoriz;
-        // check placement is set correct
-        if (['left', 'right', 'bottom', 'top'].indexOf(options.placement) === -1) {
-          options.placement = defaults.placement;
-        }
-        vertHoriz = ['left', 'right'].indexOf(options.placement) === -1 ? 'vertical' : 'horizontal';
-        // set aside classes
-        options.windowClass = 'ng-aside ' + vertHoriz + ' ' + options.placement + (options.windowClass ? ' ' + options.windowClass : '');
-        delete options.placement;
-        return $modal.open(options);
-      }
-    },
+         asideFactory = {
+       // override open method
+       open: function(config) {
+         var options = angular.extend({}, defaults, config),
+             vertHoriz;
+         // check placement is set correct
+         if (['left', 'right', 'bottom', 'top'].indexOf(options.placement) === -1) {
+           options.placement = defaults.placement;
+         }
+         vertHoriz = ['left', 'right'].indexOf(options.placement) === -1 ? 'vertical' : 'horizontal';
+         // set aside classes
+         options.windowClass = 'ng-aside ' + vertHoriz + ' ' + options.placement + (options.windowClass ? ' ' + options.windowClass : '');
+         delete options.placement;
+         return $modal.open(options);
+       }
+     },
 
-    // create $aside as extended $modal
-        $aside = angular.extend({}, $modal, asideFactory);
+     // create $aside as extended $modal
+         $aside = angular.extend({}, $modal, asideFactory);
 
-    return $aside;
+     return $aside;
   });
