95f7e453d331c85ff76ac4110cc0179e7262e127
Jeff Turner <Jeffrey.L.Turner@me.com>
2015-01-28 11:56:59 -0800

    added use strict

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,8 @@
 // Karma configuration
 // http://karma-runner.github.io/0.10/config/configuration-file.html
 
-module.exports = function (config) {
+module.exports = function (config) { 
+  'use strict';
   config.set({
         // base path, that will be used to resolve files and exclude
         basePath: '',
